8090209a20e8e3fe572f81782a1b29fda743c3a9
nikolina-marinova3 <56189511+nikolina-marinova3@users.noreply.github.com>
2020-06-20 22:22:38 +0100

    Updates

---
+++ precache-manifest.7071301d24c65fba3f773f2ef8c6db47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c322626a51c24dd069eacd1cebfc19a",
+    "url": "/react-ii/index.html"
+  },
+  {
+    "revision": "1efdfc0bd8ce6b43dbe9",
+    "url": "/react-ii/static/css/main.bb8795e1.chunk.css"
+  },
+  {
+    "revision": "e339c0e5cdd28e99d46f",
+    "url": "/react-ii/static/js/2.51ec5ce8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-ii/static/js/2.51ec5ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efdfc0bd8ce6b43dbe9",
+    "url": "/react-ii/static/js/main.6e987eb9.chunk.js"
+  },
+  {
+    "revision": "dd33969f0896d5228dd4",
+    "url": "/react-ii/static/js/runtime-main.2a3e9ef3.js"
+  }
+]);
